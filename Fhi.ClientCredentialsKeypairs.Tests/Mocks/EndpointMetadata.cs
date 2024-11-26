1de7e641b0894bb27ec5f9f795c8b7403c9be13f
GeirMG <geir.marius.gjul@fhi.no>
2024-11-26 14:13:18 +0100

    Solve warning CS8618 and set treatwarningsaserrors = true (#124)

--- Fhi.ClientCredentialsKeypairs.Tests/Mocks/EndpointMetadata.cs
+++ Fhi.ClientCredentialsKeypairs.Tests/Mocks/EndpointMetadata.cs
@@ -5,8 +5,5 @@ namespace Fhi.ClientCredentialsKeypairs.Tests.Mocks;
 public class OidcMetadata
 {
     [JsonPropertyName("token_endpoint")]
-    public string TokenEndpoint { get; set; }
-
-    [JsonPropertyName("authorization_endpoint")]
-    public string AuthorizationEndpoint { get; set; }
+    public string TokenEndpoint { get; set; } = string.Empty;
 }
