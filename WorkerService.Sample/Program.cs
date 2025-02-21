55cd9c674a0628aeafcc0cf94030f8eabb82a93e
Jenny Beate Hougen <Jenny.Beate.Hougen@fhi.no>
2025-02-21 17:03:01 +0100

    Add refit sample

--- WorkerService.Sample/Program.cs
+++ WorkerService.Sample/Program.cs
@@ -1,10 +1,14 @@
+using Fhi.ClientCredentials.Refit;
 using Fhi.ClientCredentialsKeypairs;
+using Refit;
 using WorkerService;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
 
 var clientCredentialsConfiguration = builder.Services.AddClientCredentialsKeypairs(builder.Configuration);
+
+//Using HttpClient
 builder.Services.AddHttpClient("Weather", c =>
 {
     c.BaseAddress = new Uri("https://localhost:7084");
@@ -15,6 +19,17 @@ builder.Services.AddHttpClient("Weather", c =>
     Url = "https://localhost:7084"
 });
 
+//Using Refit
+builder.Services.AddClientCredentialsRefitBuilder(clientCredentialsConfiguration)
+    .AddRefitClient<IWeatherForcastApi>();
+
 
 var host = builder.Build();
 host.Run();
+
+public record WeatherForcastModel(DateOnly Date, int TemperatureC, int TemperatureF, string? Summary);
+public interface IWeatherForcastApi
+{
+    [Get("/weatherforecast")]
+    Task<ApiResponse<IEnumerable<WeatherForcastModel>>> GetWeatherForcast();
+}
--- WorkerService.Sample/Worker.cs
+++ WorkerService.Sample/Worker.cs
@@ -1,22 +1,30 @@
+using System.Text.Json;
+
 namespace WorkerService
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
         private readonly IHttpClientFactory _factory;
+        private readonly IWeatherForcastApi _weatherForcastApi;
 
-        public Worker(ILogger<Worker> logger, IHttpClientFactory factory)
+        public Worker(ILogger<Worker> logger, IHttpClientFactory factory, IWeatherForcastApi weatherForcastApi)
         {
             _logger = logger;
             _factory = factory;
+            _weatherForcastApi = weatherForcastApi;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             var client = _factory.CreateClient("Weather");
-            var response = await client.GetAsync("/WeatherForecast");
-            var content = await response.Content.ReadAsStringAsync();
-            _logger.LogInformation(content);
+            var weatherResponseHttpClient = await client.GetAsync("/WeatherForecast");
+            var contentHttpClient = await weatherResponseHttpClient.Content.ReadAsStringAsync();
+            _logger.LogInformation(contentHttpClient);
+
+            var weatherResponseRefit = await _weatherForcastApi.GetWeatherForcast();
+            _logger.LogInformation(JsonSerializer.Serialize(weatherResponseRefit.Content));
+
             while (!stoppingToken.IsCancellationRequested)
             {
                 if (_logger.IsEnabled(LogLevel.Information))
